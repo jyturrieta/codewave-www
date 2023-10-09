f468949e7bce019b6be86b5e45d517407dd8342c
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2023-10-09 09:45:53 -0300

    cambio dependencia react-scripts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { Box } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      
+      <Box sx={{ bgcolor: 'primary.main', height: '100vh' }}> Hola </Box>
     </div>
   );
 }
