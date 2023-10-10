fbd05f2c4ff9c345fc40ddba69013d9fd49c9f2d
MagaliAcosta <magali_solacosta@hotmail.com>
2023-10-10 10:27:44 -0300

    header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import Footer from "./components/Footer";
 import Nosotros from "./components/Nosotros";
+import Header from "./components/Header";
 
 import ContactForm from "./components/ContactForm.jsx"
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Box sx={{ color:"secondary"}}>
+        <Header/>
         <Typography variant="h1" color="relaxed.main"> 
           Hello World!
         </Typography>
