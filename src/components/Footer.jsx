3c2a27571f81294235aef2eb41650bd44a17b777
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2023-10-11 09:20:47 -0300

    cambios en sm y md

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
   return (
     <>
       <Box bgcolor="grey">
-        <Box sx={{ display: "flex", flexDirection: { xs: "column", sm: "row" }, justifyContent:"space-evenly", alignItems:"center" }}>
+        <Box sx={{ display: "flex", flexDirection: { xs:"column", sm: "column", md: "row" }, justifyContent:"space-evenly", alignItems:"center" }}>
           <Box
             component="img"
             sx={{ margin: "2rem", maxWidth: "100%", height: "auto" }}
@@ -28,21 +28,21 @@ const Footer = () => {
             alt="logoBlanco"
           />
           <Box sx={{margin:"2rem", display:"flex", flexDirection:"column"}}>
-            <Box sx={{ display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"row"} }}>
+            <Box sx={{ display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
               <Email sx={{ color: "white.main", fontSize: "3rem" }} />
               <Typography variant="h6" align="center" color="white.main">
                 
                 CODEWAVE@CODEWAVE.COM.AR
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"row"} }}>
+            <Box sx={{ display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
               <Phone sx={{ color: "white.main", fontSize: "3rem" }} />
               <Typography variant="h6" align="center" color="white.main">
                 
                 +54 9 11 1234-5678
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"row"} }}>
+            <Box sx={{ display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
               <Place sx={{ color: "white.main", fontSize: "3rem" }} />
               <Typography variant="h6" align="center" color="white.main">
                 
