80fcef7d7e7fbd59e4322e65af244b6f69faa538
MagaliAcosta <magali_solacosta@hotmail.com>
2023-10-11 20:30:58 -0300

    cambios en footer y form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,28 +23,28 @@ const Footer = () => {
         <Box sx={{ display: "flex", flexDirection: { xs:"column", sm: "column", md: "row" }, justifyContent:"space-evenly", alignItems:"center" }}>
           <Box
             component="img"
-            sx={{ margin: "2rem", maxWidth: "100%", height: "auto" }}
+            sx={{ margin: "1rem", maxWidth: "30%", height: "auto" }}
             src={logoBlanco}
             alt="logoBlanco"
           />
           <Box sx={{margin:"2rem", display:"flex", flexDirection:"column"}}>
-            <Box sx={{ display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
+            <Box sx={{paddingBottom:2, display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
               <Email sx={{ color: "white.main", fontSize: "3rem" }} />
-              <Typography variant="h6" align="center" color="white.main">
+              <Typography pl={3} variant="h6" align="center" color="white.main">
                 
                 CODEWAVE@CODEWAVE.COM.AR
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
+            <Box sx={{paddingBottom:2, display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
               <Phone sx={{ color: "white.main", fontSize: "3rem" }} />
-              <Typography variant="h6" align="center" color="white.main">
+              <Typography pl={3} variant="h6" align="center" color="white.main">
                 
                 +54 9 11 1234-5678
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
+            <Box sx={{paddingBottom:2, display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
               <Place sx={{ color: "white.main", fontSize: "3rem" }} />
-              <Typography variant="h6" align="center" color="white.main">
+              <Typography pl={3} variant="h6" align="center" color="white.main">
                 
                 ORIGONE 151, HURLINGHAM
               </Typography>
@@ -58,11 +58,11 @@ const Footer = () => {
           © CODEWAVE {getYear()}
           </Typography>
         </Box>
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          <Facebook sx={{  fontSize: "4rem" }} />
-          <Instagram sx={{ fontSize: "4rem" }} />
-          <Twitter sx={{fontSize: "4rem" }} />
-        <LinkedIn sx={{ fontSize: "4rem" }} />
+        <Box sx={{ display: "flex", justifyContent: "center"}}>
+          <Facebook sx={{  fontSize: "3rem" }} />
+          <Instagram sx={{ fontSize: "3rem" }} />
+          <Twitter sx={{fontSize: "3rem" }} />
+        <LinkedIn sx={{ fontSize: "3rem" }} />
         </Box>
       </Box>
     </>
