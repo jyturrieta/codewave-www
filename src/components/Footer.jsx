37922f201d7e9cb6bfb188c1fc4a802115e83021
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2023-10-12 09:50:30 -0300

    cambios en responsive y formulario

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,28 +23,27 @@ const Footer = () => {
         <Box sx={{ display: "flex", flexDirection: { xs:"column", sm: "column", md: "row" }, justifyContent:"space-evenly", alignItems:"center" }}>
           <Box
             component="img"
-            sx={{ margin: "1rem", maxWidth: "30%", height: "auto" }}
+            sx={{ margin: "1rem", maxWidth:{xs:"70%", sm:"30%"}, height: "auto" }}
             src={logoBlanco}
             alt="logoBlanco"
           />
           <Box sx={{margin:"2rem", display:"flex", flexDirection:"column"}}>
             <Box sx={{paddingBottom:2, display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
               <Email sx={{ color: "white.main", fontSize: "3rem" }} />
-              <Typography pl={3} variant="h6" align="center" color="white.main">
-                
+              <Typography  variant="h6" align="center" color="white.main" sx={{paddingLeft:{sm:"0.5rem"}}}>
                 CODEWAVE@CODEWAVE.COM.AR
               </Typography>
             </Box>
             <Box sx={{paddingBottom:2, display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
               <Phone sx={{ color: "white.main", fontSize: "3rem" }} />
-              <Typography pl={3} variant="h6" align="center" color="white.main">
+              <Typography variant="h6" align="center" color="white.main" sx={{paddingLeft:{sm:"0.5rem"}}}>
                 
                 +54 9 11 1234-5678
               </Typography>
             </Box>
             <Box sx={{paddingBottom:2, display: "flex", alignItems: "center", flexDirection:{xs:"column", sm:"column", md:"row"} }}>
               <Place sx={{ color: "white.main", fontSize: "3rem" }} />
-              <Typography pl={3} variant="h6" align="center" color="white.main">
+              <Typography  variant="h6" align="center" color="white.main" sx={{paddingLeft:{sm:"0.5rem"}}}>
                 
                 ORIGONE 151, HURLINGHAM
               </Typography>
