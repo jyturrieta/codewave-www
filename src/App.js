b5ea92b4e14eb11294e28205a822601d3bb2a4f3
MagaliAcosta <magali_solacosta@hotmail.com>
2023-10-12 10:21:37 -0300

    links redes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import Footer from "./components/Footer";
 import Nosotros from "./components/Nosotros";
 import Header from "./components/Header";
