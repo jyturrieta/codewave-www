4d1544a075a767820b6d0af73d630df7de706918
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2023-10-20 10:03:04 -0300

    Update Servicios.jsx

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -7,144 +7,144 @@ const Servicios = () => {
 
   return (
     <>
-    <Box id="servicios"  bgcolor="primary.dark">
-        <Box sx={{padding:"2rem"}}>
-            <Typography variant="h4" align="center" color="white.main" >
-                NUESTROS SERVICIOS
-            </Typography>
+        <Box id="servicios"  bgcolor="primary.dark" sx={{padding:"1rem"}}>
+            <Box>
+                <Typography variant="h4" align="center" color="white.main" sx={{padding:"1rem"}}  >
+                    NUESTROS SERVICIOS
+                </Typography>
+            </Box>
+            <Box display="grid" justifyContent="center">
+                <Grid container spacing={2} maxWidth={'md'} >
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
+                            <Box>
+                                <SecurityIcon  color="white"/>
+                            </Box>
+                            <Box>
+                                <Typography variant="h6" color="white.main">
+                                    Seguridad
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
+                            <Box>
+                                <SecurityIcon  color="white"/>
+                            </Box>
+                            <Box>
+                                <Typography variant="h6" color="white.main">
+                                    Seguridad
+                                </Typography>
+                            </Box>
+                        
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
+                            <Box>
+                                <SecurityIcon  color="white"/>
+                            </Box>
+                            <Box>
+                                <Typography variant="h6" color="white.main">
+                                    Seguridad
+                                </Typography>
+                            </Box>
+                        
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
+                            <Box>
+                                <SecurityIcon  color="white"/>
+                            </Box>
+                            <Box>
+                                <Typography variant="h6" color="white.main">
+                                    Seguridad
+                                </Typography>
+                            </Box>
+                        
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
+                            <Box>
+                                <SecurityIcon  color="white"/>
+                            </Box>
+                            <Box>
+                                <Typography variant="h6" color="white.main">
+                                    Seguridad
+                                </Typography>
+                            </Box>
+                        
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
+                            <Box>
+                                <SecurityIcon  color="white"/>
+                            </Box>
+                            <Box>
+                                <Typography variant="h6" color="white.main">
+                                    Seguridad
+                                </Typography>
+                            </Box>
+                        
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
+                            <Box>
+                                <SecurityIcon  color="white"/>
+                            </Box>
+                            <Box>
+                                <Typography variant="h6" color="white.main">
+                                    Seguridad
+                                </Typography>
+                            </Box>
+                        
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
+                            <Box>
+                                <SecurityIcon  color="white"/>
+                            </Box>
+                            <Box>
+                                <Typography variant="h6" color="white.main">
+                                    Seguridad
+                                </Typography>
+                            </Box>
+                        
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
+                            <Box>
+                                <SecurityIcon  color="white"/>
+                            </Box>
+                            <Box>
+                                <Typography variant="h6" color="white.main">
+                                    Seguridad
+                                </Typography>
+                            </Box>
+                        
+                        </Box>
+                    </Grid><Grid item xs={12} sm={6} md={6}>
+                        <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
+                            <Box>
+                                <SecurityIcon  color="white"/>
+                            </Box>
+                            <Box>
+                                <Typography variant="h6" color="white.main">
+                                    Seguridad
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
         </Box>
-        <Box display="grid" justifyContent="center">
-            <Grid container spacing={2} maxWidth={'md'} >
-                <Grid item xs={12} sm={6} md={6}>
-                    <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
-                        <Box>
-                            <SecurityIcon  color="white"/>
-                        </Box>
-                        <Box>
-                            <Typography variant="h6" color="white.main">
-                                Seguridad
-                            </Typography>
-                        </Box>
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                    <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
-                        <Box>
-                            <SecurityIcon  color="white"/>
-                        </Box>
-                        <Box>
-                            <Typography variant="h6" color="white.main">
-                                Seguridad
-                            </Typography>
-                        </Box>
-                    
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                    <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
-                        <Box>
-                            <SecurityIcon  color="white"/>
-                        </Box>
-                        <Box>
-                            <Typography variant="h6" color="white.main">
-                                Seguridad
-                            </Typography>
-                        </Box>
-                    
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                    <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
-                        <Box>
-                            <SecurityIcon  color="white"/>
-                        </Box>
-                        <Box>
-                            <Typography variant="h6" color="white.main">
-                                Seguridad
-                            </Typography>
-                        </Box>
-                    
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                    <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
-                        <Box>
-                            <SecurityIcon  color="white"/>
-                        </Box>
-                        <Box>
-                            <Typography variant="h6" color="white.main">
-                                Seguridad
-                            </Typography>
-                        </Box>
-                    
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                    <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
-                        <Box>
-                            <SecurityIcon  color="white"/>
-                        </Box>
-                        <Box>
-                            <Typography variant="h6" color="white.main">
-                                Seguridad
-                            </Typography>
-                        </Box>
-                    
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                    <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
-                        <Box>
-                            <SecurityIcon  color="white"/>
-                        </Box>
-                        <Box>
-                            <Typography variant="h6" color="white.main">
-                                Seguridad
-                            </Typography>
-                        </Box>
-                    
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                    <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
-                        <Box>
-                            <SecurityIcon  color="white"/>
-                        </Box>
-                        <Box>
-                            <Typography variant="h6" color="white.main">
-                                Seguridad
-                            </Typography>
-                        </Box>
-                    
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                    <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
-                        <Box>
-                            <SecurityIcon  color="white"/>
-                        </Box>
-                        <Box>
-                            <Typography variant="h6" color="white.main">
-                                Seguridad
-                            </Typography>
-                        </Box>
-                    
-                    </Box>
-                </Grid><Grid item xs={12} sm={6} md={6}>
-                    <Box display="flex" justifyContent="center" flexDirection="row" alignItems="center">
-                        <Box>
-                            <SecurityIcon  color="white"/>
-                        </Box>
-                        <Box>
-                            <Typography variant="h6" color="white.main">
-                                Seguridad
-                            </Typography>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </Box>
-    </Box>
     </>
   );
 }
